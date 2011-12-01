62b3dcb6b07152f9eea4376b93fe5a23d12cd46b
root <root@OpenELEC-dev.(none)>
2011-12-01 23:56:52 +0000

    Improved the spotyxbmc2 patchfile creation script. It is now fully automated.

--- packages/mediacenter/xbmc/spotify_patches/HOW-TO-create-patchfile.sh
+++ packages/mediacenter/xbmc/spotify_patches/HOW-TO-create-patchfile.sh
@@ -1,70 +1,69 @@
 #!/bin/sh
 
-#Get the official XBMC source
-git clone git://github.com/xbmc/xbmc.git /tmp/xbmc_source
+echo Please wait..
+echo
 
-# change directory to the xbmc source code repository
-cd /tmp/xbmc_source
-git pull
+#If not done before, clone the official XBMC source to local repository
+if [ -e /$HOME/patchcreate_xbmc_source ]
+then
+  # This script have been run before, we only need to pull the latest xbmc source code from repository.
+  cd /$HOME/patchcreate_xbmc_source
+  git pull
+else
+  # First time we run this script, we need to clone the complete xbmc source code repository.
+  git clone git://github.com/xbmc/xbmc.git /$HOME/patchcreate_xbmc_source
+  cd /$HOME/patchcreate_xbmc_source
 
-# Create a branch based on the exact xbmc version that openelec currently uses
-# When creating the pathc, this is the branch we will be comparing with.
-git checkout -b xbmc_openelec c68ef0b1daed399fd88638d62646d5531bc12cae
-# replace b44b653 above with the commit id that is used by openelec
-# The commit used by Openelev can be seen in the openelec source file, packages/mediacenter/xbmc/meta
+  # Add spotyxbmc2 remote repository to our local repository:
+  git remote add spotyxbmc2 git://github.com/akezeke/spotyxbmc2.git
+fi
 
-#Add spotyxbmc2 to our repository:
-git remote add spotyxbmc2 git://github.com/akezeke/spotyxbmc2.git
+# Update with the latest source code from the remote spotyxbmc2 repository.
 git fetch spotyxbmc2
+
+# Create a branch with the latest spotyxbmc2 source code.
 git checkout --track -b spotyxbmc2 spotyxbmc2/master
 
-# ----Optional step-----
-# This can be done if you want to avoid updating xbmc to the latest spotyxbmc2 version:
-# The goal with this optional step is to create a branch with the same spotyxbmc2 date/time
-# as the xbmc version that is currently used by openelec.
-# You can easily Compare latest entries in git log to find a suitable commit in the spotyxbmc2
-# branch that are that have the same message as the latest commin in our xbmc_openelec branch.
-#
-# git log xbmc_openelec
-# git log
-# git checkout xxxxxxx
-#
-# (replace xxxxxxx above with your desired commit)
-# --End of optional step--
+# Need to remember the last commit in spotyxbmc2 source for our patch filename.
+SPOTYXBMC2_LAST_COMMIT=$(git log | head -n 1 | cut -c 8-17)
 
-# Create a branch for the spotyxbmc2 version that we will use in the patch.
-git checkout -b spotyxbmc2_patch_baseline
+# Create a branch at the last xbmc commit that is also included in spotyxbmc2 fork.
+git checkout -b last_common $(git merge-base master spotyxbmc2)
 
-# Switch to openelec branch:
-git checkout xbmc_openelec
-
-# We need a temporary branch that we will use for the patch creation.
+# We need a temporary branch that we will use for the spotyxbmc2 patch creation.
 git checkout -b tmpsquash
 
 # Merge all changes done in the spotyxbmc source code to our temporary branch.
-# Note: If conflicts are reported by this git merge, you must resolve it manually
-# by editing the relevant files. (When I did this, I did not not get any conflicts)
-git merge --squash spotyxbmc2_patch_baseline
+git merge --squash spotyxbmc2
 
-#Commit our merge. The message text here will become our patch filename later.
-# For me, 560af9fc7b was the last commit that I had in the branch spotyxbmc2_patch_baseline.
-# You can use "git log spotyxbmc_patch_baseline" to find the last commit.
-git commit -a -m "spotyxbmc2-560af9fc7b-Spotify-support-for-xbmc"
+# Commit our merge. The message text here will become our patch filename later.
+git commit -a -m "spotyxbmc2-$SPOTYXBMC2_LAST_COMMIT-Spotify-support-for-xbmc"
 
 # Create our patch (we are excluding any changes made to confluence skin):
-git format-patch --unified=3 xbmc_openelec -- $(git ls-files | grep -v -e skin.confluence -e project)
+git format-patch --unified=3 last_common -- $(git ls-files | grep -v -e skin.confluence -e project)
 
-# As a note, the only change in the skin is addition of a audio flagging icon.
+# As a sidenote, the only spoyxbmc2 change in the skin is addition of a audio flagging icon.
 # When compiling, this icon image will be copied by the openelec skin install script.
 
 cd -
-mv /tmp/xbmc_source/00*.patch .
+mv /$HOME/patchcreate_xbmc_source/00*.patch .
 
-#cleanup
-cd /tmp/xbmc_source
-git checkout  master
-git branch -D  spotyxbmc2
-git branch -D  spotyxbmc2_patch_baseline
-git branch -D  tmpsquash
-git branch -D  xbmc_openelec
+# Cleanup, here we prepare so we can run this script again.
+cd /$HOME/patchcreate_xbmc_source
+git checkout master
+git branch -D spotyxbmc2 tmpsquash last_common
 cd -
+
+echo
+echo =============================================================================
+echo
+echo Script is now finished. Please look for you patchfile in this directory:
+pwd
+echo
+echo A repository that includes the last official xbmc source code was created in:
+echo /$HOME/patchcreate_xbmc_source
+echo You might want to delete the above directory to save some disk space.
+echo If you keep the folder untouched, it will greatly reduce the time and effort
+echo to run this script again some other day in the future.
+echo =============================================================================
+
