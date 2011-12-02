30726b92cf1b55818ee327cf13d0e5b3bf9650dc
root <root@OpenELEC-dev.(none)>
2011-12-02 15:43:10 +0000

    cosmetics

--- packages/mediacenter/xbmc/spotify_patches/HOW-TO-create-patchfile.sh
+++ packages/mediacenter/xbmc/spotify_patches/HOW-TO-create-patchfile.sh
@@ -7,12 +7,12 @@ echo
 if [ -e /$HOME/patchcreate_xbmc_source ]
 then
   # This script have been run before, we only need to pull the latest xbmc source code from repository.
-  cd /$HOME/patchcreate_xbmc_source
+  cd $HOME/patchcreate_xbmc_source
   git pull
 else
   # First time we run this script, we need to clone the complete xbmc source code repository.
-  git clone git://github.com/xbmc/xbmc.git /$HOME/patchcreate_xbmc_source
-  cd /$HOME/patchcreate_xbmc_source
+  git clone git://github.com/xbmc/xbmc.git $HOME/patchcreate_xbmc_source
+  cd $HOME/patchcreate_xbmc_source
 
   # Add spotyxbmc2 remote repository to our local repository:
   git remote add spotyxbmc2 git://github.com/akezeke/spotyxbmc2.git
@@ -46,10 +46,10 @@ git format-patch --unified=3 last_common -- $(git ls-files | grep -v -e skin.con
 # When compiling, this icon image will be copied by the openelec skin install script.
 
 cd -
-mv /$HOME/patchcreate_xbmc_source/00*.patch .
+mv $HOME/patchcreate_xbmc_source/00*.patch .
 
 # Cleanup, here we prepare so we can run this script again.
-cd /$HOME/patchcreate_xbmc_source
+cd $HOME/patchcreate_xbmc_source
 git checkout master
 git branch -D spotyxbmc2 tmpsquash last_common
 cd -
@@ -57,13 +57,12 @@ cd -
 echo
 echo =============================================================================
 echo
-echo Script is now finished. Please look for you patchfile in this directory:
+echo Script is now finished. Please look for your patchfile in this directory:
 pwd
 echo
 echo A repository that includes the last official xbmc source code was created in:
-echo /$HOME/patchcreate_xbmc_source
+echo $HOME/patchcreate_xbmc_source
 echo You might want to delete the above directory to save some disk space.
 echo If you keep the folder untouched, it will greatly reduce the time and effort
 echo to run this script again some other day in the future.
 echo =============================================================================
-
